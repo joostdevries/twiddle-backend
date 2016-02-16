be4361f28d54800f21c66b292587ab517596eff9
Joost de Vries <joost@closealert.com>
2016-02-16 09:31:32 +0100

    Yesterdays work

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ var rsvp = require('rsvp');
 var AWS = require('aws-sdk');
 var s3 = new AWS.S3();
 var npm = require('npm');
+var stream = require('stream');
+
+// process.stdout doesnt work on lambda
+var echoStream = new stream.Writable();
+echoStream._write = function (chunk, encoding, done) {
+  console.log(chunk.toString().replace('\n',''));
+  done();
+};
+
 
 function createBuildDir(buildDirPath) {
   return new rsvp.Promise(function(resolve, reject) {
@@ -82,66 +91,65 @@ function symlinkBuildDirBowerComponents(srcDirPath, buildDirPath) {
 }
 
 function uploadToS3(srcDir, uploadPath) {
-  var jsFileStream = fs.createReadStream(path.join(srcDir, 'addons.js'));
-  var cssFileStream = fs.createReadStream(path.join(srcDir, 'addons.css'));
+  var jsUploadPromise = new rsvp.Promise(function(resolve, reject){
+    var jsFileStream = fs.createReadStream(path.join(srcDir, 'addons.js'));
+    jsFileStream.on('error', function (err) {
+      if (err) { return reject(err); }
+    });
 
-  jsFileStream.on('error', function (err) {
-    if (err) { throw err; }
-  });
-
-  jsFileStream.on('open', function () {
-    s3.putObject({
-      Bucket: 'addons-test',
-      Key: uploadPath + '/addon.js',
-      Body: jsFileStream
-    }, function (err) {
-      if (err) { throw err; }
+    jsFileStream.on('open', function () {
+      s3.putObject({
+        Bucket: 'addons-test',
+        ACL: 'public-read',
+        Key: uploadPath + '/addon.js',
+        ContentType: 'application/javascript',
+        Body: jsFileStream
+      }, function (err) {
+        if (err) { return reject(err); }
+        return resolve();
+      });
     });
   });
 
-  cssFileStream.on('error', function (err) {
-    if (err) { throw err; }
+  var cssUploadPromise = new rsvp.Promise(function(resolve, reject){
+    var cssFileStream = fs.createReadStream(path.join(srcDir, 'addons.css'));
+    cssFileStream.on('error', function (err) {
+      if (err) { return reject(err); }
+    });
+
+    cssFileStream.on('open', function () {
+      s3.putObject({
+        Bucket: 'addons-test',
+        ACL: 'public-read',
+        Key: uploadPath + '/addon.css',
+        ContentType: 'text/css',
+        Body: cssFileStream
+      }, function (err) {
+        if (err) { return reject(err); }
+        return resolve();
+      });
+    });
   });
 
-  cssFileStream.on('open', function () {
-    s3.putObject({
-      Bucket: 'addons-test',
-      Key: uploadPath + '/addon.js',
-      Body: cssFileStream
-    }, function (err) {
-      if (err) { throw err; }
+  return rsvp.all([jsUploadPromise, cssUploadPromise]);
+}
+
+function npmInstall(buildDirPath, addon, addonVersion) {
+  return new rsvp.Promise(function(resolve, reject) {
+    process.chdir(buildDirPath);
+    npm.load(function() {
+      console.log('NPM Loaded');
+      npm.config.set('save-dev', true);
+      npm.install(addon + '@' + addonVersion, function() {
+        console.log('NPM install complete');
+        resolve();
+      });
     });
   });
 }
 
-function giftwrap(addon, addonVersion, emberVersion) {
-  var buildDirPath = path.resolve('/tmp/build-' + addon + '-' + addonVersion.replace(/\./gi,'-') + '-ember-' + emberVersion.replace(/\./gi,'-'));
-  var buildOutPath = path.resolve('/tmp/' + addon + '-' + addonVersion.replace(/\./gi,'-') + '-ember-' + emberVersion.replace(/\./gi,'-'));
-  var srcDirPath = path.resolve('./ember-blueprints/' + emberVersion);
-  var s3Path = 'ember-' + emberVersion + '/' + addon + '/' + addonVersion;
-
-  console.log(buildDirPath);
-  console.log(buildOutPath);
-  console.log(srcDirPath);
-
-  createBuildDir(buildDirPath)
-    .then(copyPackageJson.bind(this, srcDirPath))
-    .then(symlinkBuildDirApp.bind(this, srcDirPath))
-    .then(symlinkBuildDirNodeModules.bind(this, srcDirPath))
-    .then(symlinkBuildDirBowerComponents.bind(this, srcDirPath))
-    .then(function(res, err) {
-      process.chdir(buildDirPath);
-       npm.load(function() {
-        console.log('NPM Loaded');
-        npm.instal(addon + '@' + addonVersion, function() {
-          console.log('NPM Install complete');
-          ember(buildDirPath, ['giftwrap', '--output-path=' + buildOutPath]).then(function() {
-            console.log('Ember command complete', buildOutPath);
-            uploadToS3(buildOutPath, s3Path);
-          });
-        });
-      });
-    });
+function giftwrap(buildDirPath, buildOutPath) {
+  return ember(buildDirPath, ['giftwrap', '--output-path=' + buildOutPath]);
 }
 
 function ember(pkgPath, command) {
@@ -157,17 +165,37 @@ function ember(pkgPath, command) {
   });
 }
 
-var stream = require('stream');
-var echoStream = new stream.Writable();
-echoStream._write = function (chunk, encoding, done) {
-  console.log(chunk.toString().replace('\n',''));
-  done();
-};
-
 exports.handler = function() {
   echoStream.write('Test stdout\n');
   echoStream.write('Test stderr\n');
-  giftwrap('ember-breadcrumbs','0.1.7','1.13.1');
+
+  var addon = 'ember-breadcrumbs';
+  var addonVersion = '0.1.7';
+  var emberVersion = '1.13.1';
+
+  var buildDirPath = path.resolve('/tmp/build-' + addon + '-' + addonVersion.replace(/\./gi,'-') + '-ember-' + emberVersion.replace(/\./gi,'-'));
+  var buildOutPath = path.resolve('/tmp/' + addon + '-' + addonVersion.replace(/\./gi,'-') + '-ember-' + emberVersion.replace(/\./gi,'-'));
+  var srcDirPath = path.resolve('./ember-blueprints/' + emberVersion);
+  var s3Path = 'ember-' + emberVersion + '/' + addon + '/' + addonVersion;
+
+  console.log(buildDirPath);
+  console.log(buildOutPath);
+  console.log(srcDirPath);
+
+  createBuildDir(buildDirPath)
+    .then(copyPackageJson.bind(this, srcDirPath))
+    .then(symlinkBuildDirApp.bind(this, srcDirPath))
+    .then(symlinkBuildDirNodeModules.bind(this, srcDirPath))
+    .then(symlinkBuildDirBowerComponents.bind(this, srcDirPath))
+    .then(npmInstall.bind(this, buildDirPath, addon, addonVersion))
+    .then(giftwrap.bind(this, buildDirPath, buildOutPath))
+    .then(uploadToS3.bind(this, buildOutPath, s3Path))
+    .then(function(res) {
+      console.log('Operations finished');
+      context.success();
+    },function(err) {
+      console.log('Error', err);
+    });
 };
 
 // exports.handler();
