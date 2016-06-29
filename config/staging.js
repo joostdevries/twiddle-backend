902226d0bfde5c5cfe61e9998266929fc0043b05
Gaurav Munjal <gaurav9576@yahoo.com>
2016-06-29 04:01:35 -0700

    Add support for Ember 2.0.3 (#7)

--- config/staging.js
+++ config/staging.js
@@ -9,7 +9,8 @@ module.exports = {
     builderTaskDefinition: 'addon-builder-staging',
     builderEmberVersions: {
       '1.13.13': /1\.13\./,
-      '2.0.0': /2\.0\./,
+      '2.0.0': /2\.0\.0/,
+      '2.0.3': /2\.0\./,
       '2.1.0': /2\.1\./,
       '2.2.0': /2\.2\./,
       '2.3.0': /2\.3\./,
